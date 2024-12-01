c60a8f623748e434badd60c044e2f552c7af7580
Douglas Paz <douglas.paz@unah.hn>
2024-12-01 04:17:00 -0600

    Update transaction.dart

--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -1,27 +1,39 @@
-class Transaction {
+class TransactionData {
+  final int? id;
   final double amount;
   final String category;
   final String date;
+  final String? description;
+  final bool isIncome;
 
-  Transaction({
+  TransactionData({
+    this.id,
     required this.amount,
     required this.category,
     required this.date,
+    this.description,
+    required this.isIncome
   });
 
   Map<String, dynamic> toMap() {
     return {
+      'id': id,
       'amount': amount,
       'category': category,
       'date': date,
+      'description': description,
+      'isIncome': isIncome ? 1 : 0
     };
   }
 
-  factory Transaction.fromMap(Map<String, dynamic> map) {
-    return Transaction(
+  factory TransactionData.fromMap(Map<String, dynamic> map) {
+    return TransactionData(
+      id: map['id'],
       amount: map['amount'],
       category: map['category'],
       date: map['date'],
+      description: map['description'],
+      isIncome: map['isIncome'] == 1
     );
   }
 }
